196a6f2fcfedc8ddf278c1b9ddf1f8605d24e76b
Sergio Fernandez <sergio@wikier.org>
2011-03-20 10:26:54 +0100

    added basic front page

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html 
+  version="HTML+RDFa 1.1"
+  xmlns:xsd="http://www.w3.org/2001/XMLSchema#"
+  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+  xmlns:dct="http://purl.org/dc/terms/"
+  xmlns:foaf="http://xmlns.com/foaf/0.1/"
+  xmlns:sioc="http://rdfs.org/sioc/ns#" 
+  lang="en"
+>
+
+  <head>
+    <title property="dc:title">Linked Open Colors</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="transformation" href="http://www-sop.inria.fr/acacia/soft/RDFa2RDFXML.xsl"/>
+    <link rel="stylesheet" type="text/css" href="resources/style.css" />
+    <link rel="foaf:maker" type="application/rdf+xml" title="foaf:maker" href="http://dayures.net/foaf.rdf#me" />
+    <link rel="foaf:maker" type="application/rdf+xml" title="foaf:maker" href="http://www.iminguez.net/foaf.rdf#me" />
+    <link rel="foaf:maker" type="application/rdf+xml" title="foaf:maker" href="http://www.wikier.org/foaf#wikier" />
+  </head>
+
+  <body typeof="foaf:Document" about="">
+
+    <h1>Linked Open Colors</h1>
+
+    <p>
+      @@TODO@@
+    </p>
+
+    <div id="footer">
+      This dataset has been created for the April Fools' Day of 2011, 
+      as an informal initiative for promoting Linked Data.
+    </di>
+
+  </body>
+
+</html>
+
