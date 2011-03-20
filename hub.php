7db7f5e7431970175c76d693f9b53c7afb444651
Sergio Fernandez <sergio@wikier.org>
2011-03-20 10:52:16 +0100

    implemented suffix selection based on the accept header

--- hub.php
+++ hub.php
@@ -1,13 +1,27 @@
 <?php
 
+$equivalences = array(
+                    "text/html"             => "html",
+                    "application/xhtml+xml" => "html",
+                    "application/rdf+xml"   => "rdf",
+                    "text/rdf+n3"           => "rdf",
+                    "text/turtle"           => "rdf",
+                    "application/x-turtle"  => "rdf"
+                    
+                );
+
 function get_suffix() {
     require_once "inc/conNeg/conNeg.inc.php";
-    $charsetBest = conNeg::charBest();
-    return $charsetBest; //FIXME: convert from charset to suffix
+    $mimeBest = conNeg::mimeBest();
+    if (array_key_exists($mimeBest, $equivalences)) {
+        return $equivalences[$mimeBest];
+    } else {
+        return "html";
+    }
 }
 
 $color = $_GET["color"];
-$suffix =get_suffix();
+$suffix = get_suffix();
 
 echo $color . " as " . $suffix;
 
