616e12a329fc1313c5008fe6d6e977779430ad97
Sergio Fernandez <sergio@wikier.org>
2011-03-20 12:18:37 +0100

    added empty line on the header

--- rdf.php
+++ rdf.php
@@ -1,5 +1,8 @@
-<?php header("Content-Type: application/rdf+xml; UTF-8");
-echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
+<?php
+header("Content-Type: application/rdf+xml; UTF-8");
+echo '<?xml version="1.0" encoding="utf-8"?>'; 
+?>
+
 <rdf:RDF
     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
     xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
