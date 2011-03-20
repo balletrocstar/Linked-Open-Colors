66c29e81863ad42903a0b3d46ef0d7fc828ff590
Sergio Fernandez <sergio@wikier.org>
2011-03-20 10:58:09 +0100

    fixed problem with the scope of a var

--- hub.php
+++ hub.php
@@ -1,18 +1,17 @@
 <?php
 
-$equivalences = array(
-                    "text/html"             => "html",
-                    "application/xhtml+xml" => "html",
-                    "application/rdf+xml"   => "rdf",
-                    "text/rdf+n3"           => "rdf",
-                    "text/turtle"           => "rdf",
-                    "application/x-turtle"  => "rdf"
-                    
-                );
-
 function get_suffix() {
     require_once "inc/conNeg/conNeg.inc.php";
     $mimeBest = conNeg::mimeBest();
+    $equivalences = array(
+                        "text/html"             => "html",
+                        "application/xhtml+xml" => "html",
+                        "application/rdf+xml"   => "rdf",
+                        "text/rdf+n3"           => "rdf",
+                        "text/turtle"           => "rdf",
+                        "application/x-turtle"  => "rdf"
+                        
+                    );
     if (array_key_exists($mimeBest, $equivalences)) {
         return $equivalences[$mimeBest];
     } else {
