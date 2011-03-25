6bd47bb59e4ebe647c0c44e31ddbae9a74d7faae
Sergio Fernandez <sergio@wikier.org>
2011-03-25 21:58:48 +0100

    better logging

--- colors/views.py
+++ colors/views.py
@@ -6,16 +6,16 @@ from django.http import HttpResponse
 
 
 def rgb(request, rgb):
-    logging.info("Serving RGB color with content negotiation")
+    logging.info("Serving RGB color #%s with content negotiation" % rgb)
     suffix = get_preferred_suffix(request)
     url = "%s.%s" % (rgb, suffix)
     return Http303(url)
 
 def rgb_html(request, rgb):
-    logging.info("Serving RGB color as HTML")
+    logging.info("Serving RGB color #%s as HTML" % rgb)
     return HttpResponse("%s in HTML" % rgb)
 
 def rgb_rdf(request, rgb):
-    logging.info("Serving RGB color as RDF")
+    logging.info("Serving RGB color #%s as RDF" % rgb)
     return HttpResponse("%s in RDF" % rgb)
 
