caf721a019402f2de7909c3bb60682bd3180f2c4
Sergio Fernandez <sergio@wikier.org>
2011-03-28 19:05:45 +0200

    added favicon

--- urls.py
+++ urls.py
@@ -18,6 +18,7 @@ from django.http import HttpResponsePermanentRedirect
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'index.html'}),
     (r'^color', lambda request: HttpResponsePermanentRedirect("/")),
+    (r'^favicon.ico', lambda request: HttpResponsePermanentRedirect("/resources/images/favicon.ico")),
     (r'^color/rgb/(?P<color>[0-9A-F]{3})$', 'colors.views.color'),
     (r'^color/rgb/(?P<rgb>[0-9A-F]{3}).html$', 'colors.views.rgb_html'),
     (r'^color/rgb/(?P<rgb>[0-9A-F]{3}).rdf$', 'colors.views.rgb_rdf'),
